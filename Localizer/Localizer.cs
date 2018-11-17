51ae30f6b3c5245b6e0ec1d19344f6fa5c735393
JachuPL <bonkowskijan@gmail.com>
2018-11-17 23:07:53 +0100

    Base localizer project and solution

---
+++ Localizer/Localizer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace Localizer
+{
+    public class Localizer
+    {
+        private Dictionary<string, Dictionary<string, string>> _translations;
+
+        private Localizer()
+        {
+            _translations = new Dictionary<string, Dictionary<string, string>>();
+        }
+
+        public static Localizer Setup(LocalizerConfig localizerConfig)
+        {
+            Localizer instance = new Localizer
+            {
+                _translations = localizerConfig.Translations
+            };
+            return instance;
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                try
+                {
+                    return _translations[key][Thread.CurrentThread.CurrentCulture.Name];
+                }
+                catch (KeyNotFoundException)
+                {
+                    return key;
+                }
+            }
+        }
+
+        public string this[string key, string langCode]
+        {
+            get
+            {
+                try
+                {
+                    return _translations[key][langCode];
+                }
+                catch (KeyNotFoundException)
+                {
+                    return key;
+                }
+            }
+        }
+    }
+}
---
+++ Localizer/LocalizerConfig.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace Localizer
+{
+    public class LocalizerConfig
+    {
+        public LocalizerConfig(bool overrideKeyValues = false)
+        {
+            OverrideKeyValues = overrideKeyValues;
+            Translations = new Dictionary<string, Dictionary<string, string>>();
+        }
+
+        public bool OverrideKeyValues { get; set; }
+
+        internal Dictionary<string, Dictionary<string, string>> Translations { get; }
+
+        public void AddFile(string path)
+        {
+            AddFile(path, CultureInfo.CurrentCulture.Name);
+        }
+
+        public void AddFile(string path, string localeCode)
+        {
+            string fileContent = File.ReadAllText(path, Encoding.UTF8);
+            Dictionary<string, string> parsedDictionary =
+                JsonConvert.DeserializeObject<Dictionary<string, string>>(fileContent);
+            AddTranslationsToDictionary(localeCode, parsedDictionary);
+        }
+
+        private void AddTranslationsToDictionary(string localeCode, Dictionary<string, string> parsedDictionary)
+        {
+            foreach (string key in parsedDictionary.Keys)
+            {
+                if (!Translations.ContainsKey(key))
+                    Translations.Add(key, new Dictionary<string, string>());
+
+                if (Translations[key].ContainsKey(localeCode) == false)
+                    Translations[key].Add(localeCode, parsedDictionary[key]);
+
+                if (!OverrideKeyValues)
+                    continue;
+
+                Translations[key][localeCode] = parsedDictionary[key];
+            }
+        }
+    }
+}
